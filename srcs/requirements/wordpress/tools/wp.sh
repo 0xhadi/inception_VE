bb6823f5b0dcfe8eb68c59e0d1ae6b06bc296d0b
0xhadi <mohammedhadi1313@gmail.com>
2023-10-16 00:43:22 +0100

    final

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -22,20 +22,14 @@ wp core download --path=/var/www/wordpress --allow-root
 echo "                3      xxxxxxxxxxxxxxxxxxxxxx              "
 
 
-mv /wp-config.php /var/www/wordpress/ 
 
+cd /var/www/wordpress
 
-sed -i -r "s/database_name_here/$DB_NAME/1"   $WP_PATH/wp-config.php
-sed -i -r "s/username_here/$DB_USER/1"  $WP_PATH/wp-config.php
-sed -i -r "s/password_here/$DB_PWD/1"    $WP_PATH/wp-config.php
-sed -i -r "s/localhost/$DB_HOST/1"    $WP_PATH/wp-config.php
-
-
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=mariadb --allow-root --path=/var/www/wordpress
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=mariadb --allow-root 
 echo "             4         xxxxxxxxxxxxxxxxxxxxxx              "
-wp core install --url=$WP_URL --title="Inception" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root --path=/var/www/wordpress
+wp core install --url=$WP_URL --title="Inception" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root 
 echo "            5          xxxxxxxxxxxxxxxxxxxxxx              "
-wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root --path=/var/www/wordpress
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root 
 
 
 mkdir -p /run/php
